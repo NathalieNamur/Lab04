d6d8e09a810029e76c545bbf402350d80ce0e126
Nathalie <Nathalie@192.168.1.56>
2022-07-01 13:15:51 +0200

    Implementazione Pattern ORM (Connection Pooling)

--- src/main/java/it/polito/tdp/lab04/DAO/ConnectDB.java
+++ src/main/java/it/polito/tdp/lab04/DAO/ConnectDB.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 public class ConnectDB {
+	
 	
 	//1.Definire la stringa di connessione JDBC:
 	static private final String jdbcUrl = "jdbc:mysql://localhost/iscritticorsi?user=root&password=moustache";
@@ -22,7 +25,49 @@ public class ConnectDB {
 		}
 		
 	}
+	
+	
+	
 
+	
+	/**
+	
+	//CONNECTION POOLING (PATTERN ORM): 
+	
+	//I.
+	static private HikariDataSource ds = null; 
+	static private String url = "jdbc:mysql://localhost:3306/iscritticorsi?user=root&password=moustache";
+		
+	//II.
+	public static Connection getConnection() {
+			
+		//La datasource è già stata aperta (esiste)?
+			
+		//Se la ds non esiste ancora, bisogna crearla:
+			
+		if(ds==null) {
+			ds = new HikariDataSource();
+			ds.setJdbcUrl(url);
+			ds.setUsername("root");
+			ds.setPassword("moustache");
+		}
+			
+		//Per poi restituire una nuova connessione 
+		//dalla ds appena creata:
+			
+		try {
+			return ds.getConnection();
+				
+		} catch (SQLException e) {
+			System.out.println("ERRORE di connessione al database.");
+			e.printStackTrace();
+			return null;
+		}
+			
+	}
+	
+	*/
+	
 	
 	
 }
